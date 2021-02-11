bf6363878129d43449820b04addf2395e2d82c48
Tanisha Chaudhary <tanishach203@gmail.com>
2021-02-12 00:42:35 +0545

    navbar&user UI

---
+++ src/Components/userComponent/login.component.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import './login.component.css'
+export default class LoginComponent extends Component {
+    render() {
+        return (
+		
+		<section className="sign-in">
+		<div className="container">
+			<div className="signin-content">
+				<div className="signin-image">
+					<figure><img src="images/signin-image.jpg" alt="sing up image"/></figure>
+					<a href="#" className="signup-image-link">Create an account</a>
+				</div>
+
+				<div className="signin-form">
+					<h2 className="form-title">Sign up</h2>
+					<form method="POST" className="register-form" id="login-form">
+						<div className="form-group">
+							<label for="your_name"><i className="zmdi zmdi-account material-icons-name"></i></label>
+							<input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
+						</div>
+						<div className="form-group">
+							<label for="your_pass"><i className="zmdi zmdi-lock"></i></label>
+							<input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+						</div>
+						<div className="form-group">
+							<input type="checkbox" name="remember-me" id="remember-me" className="agree-term" />
+							<label for="remember-me" className="label-agree-term"><span><span></span></span>Remember me</label>
+						</div>
+						<div className="form-group form-button">
+							<input type="submit" name="signin" id="signin" className="form-submit" value="Log in"/>
+						</div>
+					</form>
+					<div className="social-login">
+						<span className="social-label">Or login with</span>
+						<ul className="socials">
+							<li><a href="#"><i className="display-flex-center zmdi zmdi-facebook"></i></a></li>
+							<li><a href="#"><i className="display-flex-center zmdi zmdi-twitter"></i></a></li>
+							<li><a href="#"><i className="display-flex-center zmdi zmdi-google"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+        )
+    }
+}
---
+++ src/Components/userComponent/register.component.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import './login.component.css'
+
+export default class RegisterComponent extends Component {
+    render() {
+        return (
+            <div>
+                 <section className="signup">
+            <div className="container">
+                <div className="signup-content">
+                    <div className="signup-form">
+                        <h2 className="form-title">Sign up</h2>
+                        <form method="POST" className="register-form" id="register-form">
+                            <div className="form-group">
+                                <label for="name"><i className="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="name" id="name" placeholder="Your Name"/>
+                            </div>
+                            <div className="form-group">
+                                <label for="email"><i className="zmdi zmdi-email"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                            </div>
+                            <div className="form-group">
+                                <label for="pass"><i className="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="pass" id="pass" placeholder="Password"/>
+                            </div>
+                            <div className="form-group">
+                                <label for="re-pass"><i className="zmdi zmdi-lock-outline"></i></label>
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
+                            </div>
+                           
+                            <div className="form-group form-button">
+                                <input type="submit" name="signup" id="signup" className="form-submit" value="Register"/>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="signup-image">
+                        <figure><img src="images/signup-image.jpg" alt="sing up image"/></figure>
+                        <a href="#" className="signup-image-link">I am already member</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+            </div>
+        )
+    }
+}
